471ded22d45748470a4138c5fca438e6d889cb8f
dardorsh <65073890+dardorsh@users.noreply.github.com>
2025-03-17 12:01:23 +0300

    fix test after review

--- src/test/java/SelenideSoftAssertions.java
+++ src/test/java/SelenideSoftAssertions.java
@@ -1,4 +1,3 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,19 +14,12 @@ public class SelenideSoftAssertions {
 
     @Test
     public void checkJunitExample() {
-        //открываем Github
         open("https://github.com");
-        //кликаем на кнопку поиска
         $("[data-target='qbsearch-input.inputButton']").click();
-        //проверяем, что отобразилась строка поиска, и вводим в неё название Selenide
         $("[data-target='query-builder.input']").shouldBe(visible).setValue("Selenide").pressEnter();
-        //выбираем первый в списке репозиторий, проверив, что ссылка корректная
         $("[data-testid='results-list']").$$("div").first().$("a").shouldHave(href("/selenide/selenide")).click();
-        //кликаем на вкладку Wiki
         $("#wiki-tab").click();
-        //проверяем, что существует раздел Soft Assertions и кликаем на него
         $("#wiki-body").$$("a").findBy(text("Soft assertions")).click();
-        //проверяем, что в разделе есть пример кода для JUnit5
         $(".markdown-body").shouldHave(text(
                 """
                         @ExtendWith({SoftAssertsExtension.class})
